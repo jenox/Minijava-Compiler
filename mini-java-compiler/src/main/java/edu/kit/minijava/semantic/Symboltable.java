3c059639059f8304f536a2e03b267ae7bedf3a68
Christian Schnorr <christian.schnorr@me.com>
2018-11-07 21:03:18 +0100

    Improve naming + preconditions

--- mini-java-compiler/src/main/java/edu/kit/minijava/semantic/Symboltable.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/semantic/Symboltable.java
@@ -10,30 +10,36 @@ public class Symboltable {
 
     private final Stack<Map<String, VariableDeclaration>> scopes = new Stack<>();
 
-    public void enterScope() {
+    public void enterNewScope() {
         this.scopes.push(new HashMap<>());
     }
 
-    public void leaveScope() {
+    public void leaveCurrentScope() {
+        assert !this.scopes.isEmpty();
+
         this.scopes.pop();
     }
 
-    public void enterDeclaration(String ident, VariableDeclaration decl) {
-        this.scopes.peek().put(ident, decl);
+    public void enterDeclaration(String identifier, VariableDeclaration declaration) {
+        assert !this.scopes.isEmpty();
+
+        // TODO: make this a checked exception (redefinition in same scope)
+        assert !this.scopes.peek().containsKey(identifier);
+
+        // TODO: where to we catch other invalid redeclarations?
+
+        this.scopes.peek().put(identifier, declaration);
     }
 
-    public Optional<VariableDeclaration> getCurrentDeclaration(String ident) {
-        for (int i = this.scopes.size() - 1; i >= 0 ; i--) {
-            VariableDeclaration temp = this.scopes.get(i).get(ident);
-            if (temp != null)
-                return Optional.of(temp);
+    public Optional<VariableDeclaration> getVisibleDeclarationForIdentifer(String identifier) {
+        for (int i = this.scopes.size() - 1; i >= 0; i--) {
+            VariableDeclaration declaration = this.scopes.get(i).get(identifier);
+
+            if (declaration != null) {
+                return Optional.of(declaration);
+            }
         }
 
         return Optional.empty();
     }
-
-    public boolean isDeclarationInCurrentScope(String ident) {
-        return this.getCurrentDeclaration(ident).isPresent();
-    }
-
 }
