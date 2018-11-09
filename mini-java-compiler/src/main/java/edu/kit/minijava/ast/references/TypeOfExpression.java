e168b2635962b3065212472e6095b592e5645618
uheai <maikwis@gmail.com>
2018-11-09 20:21:51 +0100

    implemented type of expression

--- mini-java-compiler/src/main/java/edu/kit/minijava/ast/references/TypeOfExpression.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/ast/references/TypeOfExpression.java
@@ -2,6 +2,43 @@ package edu.kit.minijava.ast.references;
 
 public class TypeOfExpression {
 
-    // TODO: Implement
-    // TODO: How do we represent null?
+    public enum Type {
+        BOOLEAN, ARITHMETIC, NULL, TYPE_REF, UNKNOWN
+    }
+
+    private Type type;
+    private TypeReference reference;
+
+    public TypeOfExpression() {
+        this.type = Type.UNKNOWN;
+    }
+
+    /**
+     * resolve type of expression to given type
+     * @param type type expression should be resolved to
+     */
+    public void resolveTo(Type type) {
+        if (type == Type.UNKNOWN) {
+            throw new IllegalArgumentException();
+        }
+
+        this.type = type;
+        this.reference = null;
+    }
+
+    /**
+     * resolve type of expression
+     * @param reference type of expression
+     */
+    public void resolveTo(TypeReference reference) {
+        if (reference == null) {
+            throw new IllegalArgumentException();
+        }
+        this.type = Type.TYPE_REF;
+        this.reference = reference;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
 }
