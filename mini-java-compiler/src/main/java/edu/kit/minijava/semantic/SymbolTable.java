f97a3fb8ef88c9fb8554c7375e5d1043b7f6d287
uheai <maikwis@gmail.com>
2018-11-17 20:02:42 +0100

    Implemented SymbolTable as StringTable

--- mini-java-compiler/src/main/java/edu/kit/minijava/semantic/SymbolTable.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/semantic/SymbolTable.java
@@ -1,45 +1,95 @@
 package edu.kit.minijava.semantic;
 
-import edu.kit.minijava.ast.nodes.*;
-
 import java.util.*;
+
+import edu.kit.minijava.ast.nodes.VariableDeclaration;
 
 // TODO: Put this logic straight into SemanticAnalysisBase?
 class SymbolTable {
-    SymbolTable() {
+
+    private final HashMap<String, VariableScopesAndDeclarations> nameToDeclarations = new HashMap<>();
+    private final HashMap<Integer, Set<String>> scopeToNames = new HashMap<>();
+
+    public SymbolTable() {
+        this.currentScope = 0;
     }
 
-    private final Stack<Map<String, VariableDeclaration>> scopes = new Stack<>();
+    private int currentScope;
 
     void enterNewScope() {
-        this.scopes.push(new HashMap<>());
+        this.currentScope++;
     }
 
     void leaveCurrentScope() {
-        assert !this.scopes.isEmpty();
+        assert this.currentScope > 0 : "left more scopes than entered";
 
-        this.scopes.pop();
+        Set<String> definedVars = this.scopeToNames.get(this.currentScope);
+
+        if (definedVars != null) {
+
+            for (String var : definedVars) {
+                VariableScopesAndDeclarations scope = this.nameToDeclarations.get(var);
+                scope.removeLastScope();
+
+                if (scope.isEmpty()) {
+                    this.nameToDeclarations.remove(var);
+                }
+            }
+
+            // remove entry from map
+            this.scopeToNames.remove(this.currentScope);
+
+        }
+
+        this.currentScope--;
+
     }
 
     void enterDeclaration(VariableDeclaration declaration) {
-        assert !this.scopes.isEmpty();
+        String name = declaration.getName();
+        if (this.nameToDeclarations.containsKey(name)) {
+            VariableScopesAndDeclarations scope = this.nameToDeclarations.get(name);
+            scope.addVariableDeclaration(declaration, this.currentScope);
+        }
 
-        this.scopes.peek().put(declaration.getName(), declaration);
+        // name is not in map
+        else {
+            VariableScopesAndDeclarations scope = new VariableScopesAndDeclarations(declaration, this.currentScope);
+            this.nameToDeclarations.put(name, scope);
+
+            // add name to scopeToNames
+            if (this.scopeToNames.containsKey(scope)) {
+                this.scopeToNames.get(scope).add(name);
+            }
+            else {
+                HashSet<String> set = new HashSet<>();
+                set.add(name);
+                this.scopeToNames.put(this.currentScope, set);
+            }
+        }
     }
 
     Optional<VariableDeclaration> getVisibleDeclarationForName(String name) {
-        for (int i = this.scopes.size() - 1; i >= 0; i--) {
-            VariableDeclaration declaration = this.scopes.get(i).get(name);
 
-            if (declaration != null) {
-                return Optional.of(declaration);
-            }
+        if (this.nameToDeclarations.containsKey(name)) {
+            VariableDeclaration declaration = this.nameToDeclarations.get(name).getLastDeclaration();
+            return Optional.of(declaration);
+        }
+        else {
+            return Optional.empty();
         }
 
-        return Optional.empty();
     }
 
     boolean isDeclarationInCurrentScope(VariableDeclaration declaration) {
-        return this.scopes.peek().containsValue(declaration);
+        String name = declaration.getName();
+        Set<String> definedVars = this.scopeToNames.get(this.currentScope);
+
+        if (definedVars == null) {
+            return false;
+        }
+        else {
+            return definedVars.contains(name);
+        }
     }
 }
