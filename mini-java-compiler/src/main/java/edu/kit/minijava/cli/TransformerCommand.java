722a7ea73bf5b177d68cac45f959fa778e11bf01
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-12-02 08:06:23 +0100

    Compile runtime and generate code from firm graphs

--- mini-java-compiler/src/main/java/edu/kit/minijava/cli/TransformerCommand.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/cli/TransformerCommand.java
@@ -24,10 +24,44 @@ public class TransformerCommand extends Command {
 
             ReferenceAndExpressionTypeResolver resolver = new ReferenceAndExpressionTypeResolver(program);
 
+            String asmOutputFilename = "a.s";
+            String executableFilename = "a.out";
 
             EntityVisitor visitor = new EntityVisitor();
-            visitor.transform(program);
+            visitor.transform(program, asmOutputFilename);
 
+            // TODO Set correct path to library
+
+            // Compile standard library
+            Process p = Runtime.getRuntime().exec("gcc -S lib/stdlib.c -o lib.s");
+            int result;
+            try {
+                result = p.waitFor();
+            }
+            catch (Throwable t) {
+                result = -1;
+            }
+
+            if (result != 0) {
+                System.err.println("error: Failed to compile standard library!");
+                return 1;
+            }
+
+            // Assemble and link runtime and code
+
+            p = Runtime.getRuntime().exec("gcc lib.s " + asmOutputFilename + " -o " + executableFilename);
+
+            try {
+                result = p.waitFor();
+            }
+            catch (Throwable t) {
+                result = -1;
+            }
+
+            if (result != 0) {
+                System.err.println("error: Linking step failed!");
+                return 1;
+            }
 
             return 0;
         }
