28db2968182b42893886c63f8e7ea7581a189b82
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-12-02 08:03:04 +0100

    Remove debug output from runtime functions

--- mini-java-compiler/lib/stdlib.c
+++ mini-java-compiler/lib/stdlib.c
@@ -3,27 +3,22 @@
 int __minijava_main(void);
 
 int main(void) {
-    printf("MiniJava runtime: Main function\n");
     __minijava_main();
     return 0;
 }
 
 int system_in_read() {
-    printf("MiniJava runtime: read\n");
     return getchar();
 }
 
 void system_out_println(int x) {
-    printf("MiniJava runtime: println\n");
     printf("%d\n", x);
 }
 
 void system_out_write(int x) {
-    printf("MiniJava runtime: write\n");
     putchar(x);
 }
 
 void system_out_flush() {
-    printf("MiniJava runtime: flush\n");
     fflush(stdout);
 }
