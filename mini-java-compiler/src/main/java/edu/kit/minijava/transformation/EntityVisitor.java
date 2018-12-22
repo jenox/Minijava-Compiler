c286a609041bb42ab85fa79709da9f15c89c1802
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-12-22 17:39:38 +0100

    Fix memory node usage for return statement generation
    This bug caused an old memory state to be used for return nodes in some
    cases, which could lead to dangling nodes in some test cases.

--- mini-java-compiler/src/main/java/edu/kit/minijava/transformation/EntityVisitor.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/transformation/EntityVisitor.java
@@ -539,7 +539,6 @@ public class EntityVisitor extends ASTVisitor<EntityContext> {
 
         if (!this.isVariableCounting) {
             Node[] results;
-            Node mem = context.getConstruction().getCurrentMem();
 
             if (context.getResult() != null) {
                 results = new Node[] {context.getResult().convertToValue().getNode()};
@@ -548,6 +547,7 @@ public class EntityVisitor extends ASTVisitor<EntityContext> {
                 results = new Node[] {};
             }
 
+            Node mem = context.getConstruction().getCurrentMem();
             Node returnNode = context.getConstruction().newReturn(mem, results);
             context.getConstruction().getGraph().getEndBlock().addPred(returnNode);
 
