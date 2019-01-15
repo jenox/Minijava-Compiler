16fccaa873066555c64742e31eef37167962c955
Maximilian Stemmer-Grabow <mail@mxsg.de>
2019-01-15 07:53:10 +0100

    Revert "Remove zero-sized heap allocation in stdlib with x86 calling convention"
    This reverts commit 55bad0fd6ee49e179dec115f21640131487bd0e3.

--- mini-java-compiler/lib/stdlib.c
+++ mini-java-compiler/lib/stdlib.c
@@ -25,15 +25,7 @@ void system_out_flush() {
 }
 
 void* alloc_mem(int num, int size) {
-    int bytes = num * size;
-
-    // allocate some multiple of quadwords (8 bytes)
-    bytes = (bytes + 8 - 1) / 8 * 8;
-
-    // prevent null pointer for zero-size types or arrays
-    bytes = (bytes == 0) ? 8 : bytes;
-
-    void* pointer = calloc(1, bytes);
+    void* pointer = calloc(num, size);
 
     // Catch null pointer on zero-sized or failed allocation
     // We abort execution at this point
