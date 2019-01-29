547540f53851db3396d75fda1ee3fb3e380e8b67
Maximilian Stemmer-Grabow <mail@mxsg.de>
2019-01-29 01:18:52 +0100

    Fix bug in PhiNode construction

--- mini-java-compiler/src/main/java/edu/kit/minijava/backend/PhiNode.java
+++ mini-java-compiler/src/main/java/edu/kit/minijava/backend/PhiNode.java
@@ -81,7 +81,7 @@ public class PhiNode {
             throw new IllegalArgumentException();
         }
 
-        this.mappings = new ArrayList<>(mappings);
+        this.mappings = mappings;
     }
 
     public BasicBlock getBasicBlock() {
