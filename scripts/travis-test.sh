865905a182708efcbe8000196ea2841312e8b7f5
Maximilian Stemmer-Grabow <mail@mxsg.de>
2019-02-03 00:03:30 +0100

    Factor out travis test script into different file

---
+++ scripts/travis-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+case "$TESTTYPE" in
+  checkstyle)
+    ./mini-java-compiler/gradlew -b ./mini-java-compiler/build.gradle checkstyleMain checkstyleTest
+    ;;
+  frontend)
+    ./build --no-swift && \
+    echo "" && echo "" && echo "#### LEXER TESTS" && \
+    ./mjtest/mjt.py lexer --ci_testing --parallel && \
+    echo "" && echo "" && echo "#### PARSER TESTS" && \
+    ./mjtest/mjt.py syntax --ci_testing --parallel && \
+    echo "" && echo "" && echo "#### AST GENERATION TESTS" && \
+    ./mjtest/mjt.py ast --ci_testing --parallel && \
+    echo "" && echo "" && echo "#### SEMANTIC TESTS" && \
+    ./mjtest/mjt.py semantic --ci_testing --parallel && \
+    echo "" && echo "" && echo "#### FIRM COMPILATION TESTS" && \
+    ./mjtest/mjt.py compile-firm --ci_testing --parallel
+    ;;
+  backend)
+    ./build && \
+    echo "" && echo "" && echo "#### COMPILATION TESTS" &&
+    ./mjtest/mjt.py compile --ci_testing --parallel && \
+    echo "" && echo "" && echo "#### COMPILATION TESTS (BIG TEST CASES)" &&
+    ./molki__build_run_check-BigTests.py &&
+    echo "" && echo "" && echo "#### COMPILATION TESTS (NO OPTIMIZATIONS)" &&
+    ./mjtest/mjt.py no-optimization --ci_testing --parallel
+    ;;
+  *)
+    echo "Unknown test type! Quitting ..."; return 1
+    ;;
+esac
