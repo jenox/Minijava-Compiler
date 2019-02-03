e9712f82f7078711b75361cd5d8a4ec5d496b422
uheai <maikwis@gmail.com>
2019-02-03 16:41:53 +0100

    implemented correct input handling

--- molki__build_run_check-BigTests.py
+++ molki__build_run_check-BigTests.py
@@ -29,7 +29,7 @@ for file_name in test_files:
         or file_name.startswith(".java")
         or file_name.startswith(".mj")
         or file_name.endswith(".input.java.out")):
-
+              
         print("current testcase: " + file_name)
         # build + run backend + molki
         run([ "./run"
@@ -40,24 +40,26 @@ for file_name in test_files:
         if file_name.endswith(".input.java"):
             input_file_name = test_dir + "/" + file_name.replace(".input.java", ".0.inputc") 
             print("input: " + input_file_name)
-           # cmd = shlex.split("./a.out < " + input_file_name)
-           # print("cmd: " + cmd)
-            #temp, _ = Popen(["./a.out", "<", input_file_name , stdout=PIPE).communicate()
-            temp = subprocess.check_output(["./a.out", "<", input_file_name])
+            input_file = open(input_file_name)
+            p = Popen(["./a.out"], stdout=PIPE, stdin=input_file, stderr=STDOUT)
+            temp, _ = p.communicate()
             tempStr = str(temp.decode()).strip()
-            
+
+            file = open(test_dir + "/" + file_name.replace(".input.java", ".0.inputc.out"))
+            input_file.close()
+            #print(tempStr)
         else: 
             print("no input")
             temp, _ = Popen([ "./a.out" ], stdout=PIPE).communicate()
             tempStr = str(temp.decode()).strip()
 
+            if os.path.isfile(test_dir + "/" + file_name + ".out"):
+                file = open(test_dir + "/" + file_name + ".out", "r")
+            else:
+                assert os.path.isfile(test_dir + "/" + file_name[:-3] + ".out"), "output file is missing"
+                file = open(test_dir + "/" + file_name[:-3] + ".out", "r")
 
-        if os.path.isfile(test_dir + "/" + file_name + ".out"):
-            file = open(test_dir + "/" + file_name + ".out", "r")
-        else:
-            assert os.path.isfile(test_dir + "/" + file_name[:-3] + ".out"), "output file is missing"
-            file = open(test_dir + "/" + file_name[:-3] + ".out", "r")
-
+        
         temp2 = file.read().strip()
         file.close()
 
@@ -70,3 +72,4 @@ for file_name in test_files:
             exit_code = 1
 
 sys.exit(exit_code)
+
